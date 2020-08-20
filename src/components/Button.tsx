cbba71878a083ac20e5c0cf3d83fe3c02a25d376
Kevin Williams <kevin@leakynoggin.com>
2020-08-20 13:42:54 -0500

    add storybook

---
+++ src/components/Button.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export interface ButtonProps {
+
+}
+
+export const Button: React.FC<ButtonProps> = () => {
+    return (
+        <button>
+            I am a button
+        </button>
+    )
+}
---
+++ src/stories/Button.stories.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+// also exported from '@storybook/react' if you can deal with breaking changes in 6.1
+import { Story, Meta } from '@storybook/react/types-6-0'
+
+// Component
+import { Button, ButtonProps} from '../components/Button'
+
+export default { title: 'Example/Button',
+     component: Button,
+  } as Meta;
+
+
+const Template: Story<ButtonProps> = (args) => <Button {...args} />;
+
+
+export const Default = Template.bind({});
+Default.args = {
+
+};
